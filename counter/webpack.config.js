3b8437ca95664e901a58363aac26cf14103f230a
Steven Jehannet <sjehann@gmail.com>
2018-04-04 22:52:45 +0200

    counter

---
+++ counter/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Knowthen/counter',
+      template: 'src/index.html'
+    })
+  ],
+  devServer: {
+    stats: 'errors-only',
+    open: true,
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
